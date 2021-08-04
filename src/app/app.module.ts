dda8ba22a67986e7dd4ac9cd80a75a06441a8f61
pablo2110 <pablopaniagua2110@gmail.com>
2021-08-03 23:17:33 -0500

    mi primer commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,22 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { HomeComponent } from './components/home/home.component';
+import { BuscadorComponent } from './components/buscador/buscador.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HomeComponent,
+    BuscadorComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
